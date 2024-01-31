1baf3fb97fba232e6ed9e4c70c4a53ffc27d7ce0
Alvin Mwakai <mwakaimwambala@gmail.com>
2024-01-31 17:56:44 +0300

    Enable viewBinding and users logout

--- app/src/main/java/com/example/inua/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/inua/fragments/ProfileFragment.kt
@@ -1,23 +1,70 @@
 package com.example.inua.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.inua.R
+import android.widget.Toast
+import com.example.inua.auth.SignIn
+import com.example.inua.databinding.FragmentProfileBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.squareup.picasso.Picasso
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val account = GoogleSignIn.getLastSignedInAccount(requireContext())
+        account?.let {
+            Picasso.get().load(it.photoUrl).into(binding.imgProfile)
+            binding.userName.text = it.displayName
+        }
+
+        binding.editProfile.setOnClickListener {
+
+        }
+
+        binding.viewDonations.setOnClickListener {
+
+        }
+
+        binding.logout.setOnClickListener {
+            GoogleSignIn.getClient(requireActivity(), GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .signOut()
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(activity, SignIn::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        startActivity(intent)
+                        activity?.finish()
+                    } else {
+                        Toast.makeText(
+                            activity,
+                            "An error occurred while trying to log out",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+
+
+    }
 
 }
