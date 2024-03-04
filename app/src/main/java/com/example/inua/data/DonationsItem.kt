e4e9f83888bdc3d14ec7d3117ab94d5a17b59a0f
Alvin Mwakai <mwakaimwambala@gmail.com>
2024-03-05 02:03:42 +0300

    added donations data class

---
+++ app/src/main/java/com/example/inua/data/DonationsItem.kt
@@ -0,0 +1,9 @@
+package com.example.inua.data
+
+data class DonationsItem(
+    val contactInfo: String,
+    val description: String,
+    val id: String,
+    val imageName: String,
+    val location: String
+)
