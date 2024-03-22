a5b0a686dbd1b73171e7627f25203b7d710a4d6d
Mwakai <mwakaimwambala@gmail.com>
2024-03-23 02:28:27 +0300

    added Parcelable

--- app/src/main/java/com/example/inua/data/ContactInfo.kt
+++ app/src/main/java/com/example/inua/data/ContactInfo.kt
@@ -1,7 +1,10 @@
 package com.example.inua.data
 
+import android.os.Parcelable
+
+@kotlinx.parcelize.Parcelize
 data class ContactInfo(
     val address: String = "",
     val email: String = "",
     val phone: String = ""
-)
+) : Parcelable
--- app/src/main/java/com/example/inua/data/Organization.kt
+++ app/src/main/java/com/example/inua/data/Organization.kt
@@ -1,5 +1,8 @@
 package com.example.inua.data
 
+import android.os.Parcelable
+
+@kotlinx.parcelize.Parcelize
 data class Organization(
     val contactInfo: ContactInfo = ContactInfo(), // Assuming ContactInfo also has a no-argument constructor
     val id: Int = 0,
@@ -8,4 +11,4 @@ data class Organization(
     val name: String = "",
     val shortDescription: String = "",
     val totalDonations: Int = 0
-)
+) : Parcelable
