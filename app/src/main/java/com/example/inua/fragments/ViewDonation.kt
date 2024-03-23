00cf9bf984110d5907bb27599464a3fc7a08f615
Mwakai <mwakaimwambala@gmail.com>
2024-03-23 03:06:53 +0300

    update

--- app/src/main/java/com/example/inua/fragments/ViewDonation.kt
+++ app/src/main/java/com/example/inua/fragments/ViewDonation.kt
@@ -5,16 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.inua.R
+import com.example.inua.data.Organization
+import com.example.inua.databinding.FragmentHomeBinding
+import com.example.inua.databinding.FragmentViewDonationBinding
+import com.squareup.picasso.Picasso
 
 class ViewDonation : Fragment() {
 
+    private var _binding: FragmentViewDonationBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_donation, container, false)
+        _binding = FragmentViewDonationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val organization = arguments?.getParcelable<Organization>("organization")
+
+        organization?.let {
+            view.donationName.text = it.name // Assuming donationName is a TextView for the name
+            view.longDescription.text = it.longDescription // Assuming longDescription is a TextView for the description
+            Picasso.get().load(it.image).into(view.imageView)
+        }
     }
 
 
