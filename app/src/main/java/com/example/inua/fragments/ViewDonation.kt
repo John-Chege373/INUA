3af0e72ba51dd7c38344abe857b61cc5a9c28423
Mwakai <mwakaimwambala@gmail.com>
2024-03-23 03:47:16 +0300

    show donation

--- app/src/main/java/com/example/inua/fragments/ViewDonation.kt
+++ app/src/main/java/com/example/inua/fragments/ViewDonation.kt
@@ -5,11 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.example.inua.R
 import com.example.inua.data.Organization
-import com.example.inua.databinding.FragmentHomeBinding
 import com.example.inua.databinding.FragmentViewDonationBinding
 import com.squareup.picasso.Picasso
 
@@ -17,12 +14,13 @@ class ViewDonation : Fragment() {
 
     private var _binding: FragmentViewDonationBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         _binding = FragmentViewDonationBinding.inflate(inflater, container, false)
+        binding.progressBar.visibility = View.VISIBLE
         return binding.root
     }
 
@@ -31,11 +29,15 @@ class ViewDonation : Fragment() {
         val organization = arguments?.getParcelable<Organization>("organization")
 
         organization?.let {
-            view.donationName.text = it.name // Assuming donationName is a TextView for the name
-            view.longDescription.text = it.longDescription // Assuming longDescription is a TextView for the description
-            Picasso.get().load(it.image).into(view.imageView)
+            binding.donationName.text = it.name
+            binding.longDescription.text = it.longDescription
+            Picasso.get().load(it.image).into(binding.imageView)
+            binding.progressBar.visibility = View.GONE
         }
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // To avoid memory leaks
+    }
 }
